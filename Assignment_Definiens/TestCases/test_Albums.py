267ea56a92c35364ede2a57b5677a0e04c437698
dob090885 <ashish.deshmukh0908@gmail.com>
2019-05-23 17:25:40 +0200

    Updating TCs with fixtures

--- Assignment_Definiens/TestCases/test_Albums.py
+++ Assignment_Definiens/TestCases/test_Albums.py
@@ -3,24 +3,22 @@ import jsonpath
 import requests
 import pytest
 
-
-@pytest.mark.Smoke
-def test_Validate_StatusCode():
-    # Send the GET request
+@pytest.fixture()
+def getresponse():
+    global response
     url = "https://jsonplaceholder.typicode.com/albums"
     response = requests.get(url)
+
+@pytest.mark.Smoke
+def test_Validate_StatusCode(getresponse):
     assert response.status_code == 200
 
 @pytest.mark.Smoke
-def test_FetchServer():
-    url = "https://jsonplaceholder.typicode.com/albums"
-    response = requests.get(url)
+def test_FetchServer(getresponse):
     assert response.headers.get('Server') == 'cloudflare'
 
 @pytest.mark.Sanity
-def test_CountUser():
-    url = "https://jsonplaceholder.typicode.com/albums"
-    response = requests.get(url)
+def test_CountUser(getresponse):
     json_response = json.loads(response.text)
     albums = jsonpath.jsonpath(json_response, '$[:]')
     assert len(albums) == 100
--- Assignment_Definiens/TestCases/test_Users.py
+++ Assignment_Definiens/TestCases/test_Users.py
@@ -3,24 +3,23 @@ import jsonpath
 import requests
 import pytest
 
+@pytest.fixture()
+def getresponse():
+    global response
+    url = "https://jsonplaceholder.typicode.com/albums"
+    response = requests.get(url)
+
 
 @pytest.mark.Smoke
-def test_Validate_StatusCode():
-    # Send the GET request
-    url = "https://jsonplaceholder.typicode.com/users"
-    response = requests.get(url)
+def test_Validate_StatusCode(getresponse):
     assert response.status_code == 200
 
 @pytest.mark.Smoke
-def test_FetchServer():
-    url = "https://jsonplaceholder.typicode.com/users"
-    response = requests.get(url)
+def test_FetchServer(getresponse):
     assert response.headers.get('Server') == 'cloudflare'
 
 @pytest.mark.Sanity
-def test_CountUser():
-    url = "https://jsonplaceholder.typicode.com/users"
-    response = requests.get(url)
+def test_CountUser(getresponse):
     json_response = json.loads(response.text)
     users = jsonpath.jsonpath(json_response, '$[:]')
     assert len(users) == 10
